ddcf7eccba5414df553f2afe429bf8eee0400fab
SEGHAIER <mohamed-seghaier@laplateforme.io>
2021-11-15 14:51:31 +0100

    day01 job01, 02, 03

---
+++ jour01/job03/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+        int a = 10;
+        int b = 2;
+
+        printf("Division : %d\n", (a / b));
+        printf("Addition : %d\n", (a + b));
+        printf("Soustraction : %d\n", (a - b));
+        printf("Multiplication : %d\n", (a * b));
+
+        return (0);
+}
+        
