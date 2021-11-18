d78c0d3391c37687ebbb2e0feeaf6954b95af76c
SEGHAIER <mohamed-seghaier@laplateforme.io>
2021-11-19 00:11:17 +0100

    push ex01

---
+++ jour03/job01/my.h
@@ -0,0 +1,12 @@
+#ifndef MY_H_
+#define MY_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int     main(void);
+void    my_putstr(char *);
+void    my_putchar(char);
+
+#endif /* !MY_H_ */
