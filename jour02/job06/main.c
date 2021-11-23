6916c756d5195a9471b9956aaac0992390924ed8
SEGHAIER <mohamed-seghaier@laplateforme.io>
2021-11-23 12:27:42 +0100

    push jour06 ex01 et 02

--- jour02/job06/main.c
+++ jour02/job06/main.c
@@ -6,15 +6,18 @@
 
 int main(void)
 {
+    return (my_ascii_to_char());
+}
+
+int my_ascii_to_char(void) {
     int i = 0;
 
     int *tab = malloc(sizeof(int) * 9);
-
+    char *str = malloc(sizeof(int) * 9);
 
 
     for (int nb = 49; nb < 58; i += 1, nb += 1) tab[i] = nb;
 
-    char *str = malloc(sizeof(char) * 9);
 
     for (i = 0; tab[i]; i += 1)
         str[i] = tab[i];
---
+++ jour06/job01/main.c
@@ -0,0 +1,9 @@
+#include "./my.h/my.h"
+
+int main(int argc, char **argv)
+{
+    printf("%d\n", argc);
+    
+    return 0;
+}
+
